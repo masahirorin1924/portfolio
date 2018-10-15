439624c5b5a3c5c6eaeb7250e088d40319170e9b
masahirorin1924 <masahiro.iida.1924@gmail.com>
2018-10-15 22:27:03 +0900

    試しにrootを外すとどうなるか

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root 'home#top'
+  #root 'home#top'
+  get 'home/top' => 'home#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
